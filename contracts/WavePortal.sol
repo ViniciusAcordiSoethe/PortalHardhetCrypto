e027516c1b829fbae9497b42a6ff631f67cb89dd
ViniciusAcordiSoethe <soethe42@gmail.com>
2023-01-30 12:07:14 -0300

    Fazer o WavePortal

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract WavePortal{
+    constructor(){
+        console.log("Ueba, eu sou um contrato e eu sou inteligente");
+        }
+}
